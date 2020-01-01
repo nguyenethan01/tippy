d05f714818209eadbb296b71bcceb3e75b07685a
nguyenethan01 <nguyenethan01@gmail.com>
2020-01-01 14:49:46 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Ethan  Nguyen on 1/1/20.
+//  Copyright © 2020 nguyenethan01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
