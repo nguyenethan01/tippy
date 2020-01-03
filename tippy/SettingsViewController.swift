6619e96e3470ce5e5f9db7fb1409ddf86a5c2ca2
nguyenethan01 <nguyenethan01@gmail.com>
2020-01-02 18:13:50 -0800

    Adding Settings View Controller

---
+++ tippy/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  tippy
+//
+//  Created by Ethan  Nguyen on 1/2/20.
+//  Copyright © 2020 nguyenethan01. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
